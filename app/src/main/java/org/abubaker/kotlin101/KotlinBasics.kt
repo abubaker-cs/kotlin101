fe6c1db8b03e9f3046ffb3b2cd0b49192eb0b475
abubaker <mohammad_abubaker@yahoo.com>
2021-03-29 21:52:51 +0500

    Studying about Integer Types

--- app/src/main/java/org/abubaker/kotlin101/KotlinBasics.kt
+++ app/src/main/java/org/abubaker/kotlin101/KotlinBasics.kt
@@ -2,4 +2,28 @@ package org.abubaker.kotlin101
 
 fun main() {
 
+    // Var = Variable | Val = Constant
+    var myName = "Abubaker Farooq"
+
+    // TODO: Add new functionality
+    print("Hello $myName")
+
+    /**
+     * This is
+     * a multiline
+     * comment.
+     */
+
+    // Byte (8 bit)
+    val myByte: Byte = 13
+
+    // Short (16 bit)
+    val myShort: Short = 125
+
+    // Int (32 bit)
+    val myInt: Int = 123123123
+
+    // Long (64 bit)
+    val myLong: Long = 12_039_921_123_121_323
+
 }
