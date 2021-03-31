dab44402b9625c3e9bae785ba2e8bf1f44fe5349
abubaker <mohammad_abubaker@yahoo.com>
2021-03-31 14:24:01 +0500

    Studying functions / methods

--- app/src/main/java/org/abubaker/kotlin101/KotlinFunctions.kt
+++ app/src/main/java/org/abubaker/kotlin101/KotlinFunctions.kt
@@ -1,9 +1,35 @@
 package org.abubaker.kotlin101
 
+/**
+ * Understanding Functions in Kotlin
+ */
 fun main() {
-    /**
-     * Understanding Functions in Kotlin
-     */
 
-    
+
+
+    // Argument (what we are sending): (5, 3)
+    // var result = addUp(5, 3)
+    var avg = avg(5.20, 13.37)
+    println("Results is $avg")
+
+    var sum = addUp(5, 3)
+    println("Sum: $sum")
+
+}
+
+// Parameter (what will be recieved) (input)
+fun addUp(a: Int, b: Int): Int {
+
+    // Output
+    return a + b
+
+}
+
+fun myFunction() {
+    println("Called from myFunction")
+}
+
+// Method - it is a function within a Class, i.e: onCreate()
+fun avg(a: Double, b: Double): Double {
+    return (a + b) / 2
 }
