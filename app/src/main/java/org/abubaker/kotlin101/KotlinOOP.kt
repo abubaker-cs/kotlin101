ad62dac37e437633331b03c86861d19a2a83d5a8
abubaker <mohammad_abubaker@yahoo.com>
2021-04-02 15:22:16 +0500

    Understanding OOP

--- app/src/main/java/org/abubaker/kotlin101/KotlinOOP.kt
+++ app/src/main/java/org/abubaker/kotlin101/KotlinOOP.kt
@@ -1,8 +1,11 @@
 package org.abubaker.kotlin101
 
 fun main() {
-    var denis: Person = Person("Denis", "Panjuta")
-    var john = Person()
+    // var denis: Person = Person("Denis", "Panjuta")
+    // var john = Person()
+    /// var johnPeterson = Person(lastName = "Peterson")
+
+    var sample = MobilePhone("Android", "Samsung", "Galaxy S20 Ultra")
 }
 
 class Person constructor(firstName: String = "John", lastName: String = "Doe") {
@@ -10,9 +13,21 @@ class Person constructor(firstName: String = "John", lastName: String = "Doe") {
     // It will be initialized whenever the Person will be created
     init {
         println(
-            "Initalized a new Person Object with:" +
+            "Initialized a new Person Object with:" +
                     "\nFirst Name = $firstName \nLast Name = $lastName \n"
         )
     }
 
+}
+
+class MobilePhone constructor(osName: String, brand: String, model: String) {
+
+    init {
+        println(
+            "$brand $model. \n" +
+            "Here the osName is $osName, " +
+                    "brand is $brand " +
+                    "model is $model \n"
+        )
+    }
 }
